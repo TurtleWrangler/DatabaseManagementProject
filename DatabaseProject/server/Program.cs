a7dda0e8f0e20c6ec2fbc21ae254eca2f4f27fec
randobrando1008 <brandongregory1998@gmail.com>
2021-10-31 12:28:54 -0400

    Connection to database

---
+++ DatabaseProject/server/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Data;
+
+using MySql.Data;
+using MySql.Data.MySqlClient;
+
+namespace server
+{
+    public class MySqlConn
+    {
+        public static void Main()
+        {
+            string connStr = "server=localhost;user=root;database=testdata;port=3306;password=password";
+            MySqlConnection conn = new MySqlConnection(connStr);
+            try
+            {
+                Console.WriteLine("Connecting to MySQL...");
+                conn.Open();
+
+                string sql = "SELECT * FROM testinformation";
+                MySqlCommand cmd = new MySqlCommand(sql, conn);
+                MySqlDataReader rdr = cmd.ExecuteReader();
+
+                while (rdr.Read())
+                {
+                    Console.WriteLine(rdr[0] + " -- " + rdr[1]);
+                }
+                rdr.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+
+            conn.Close();
+            Console.WriteLine("Done.");
+        }
+    }
+}
+
