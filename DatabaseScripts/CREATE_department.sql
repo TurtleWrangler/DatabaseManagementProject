5c97cd70d9fe5c023927aa49a656534b2f7efdc1
Derek Dunn <32466866+TurtleWrangler@users.noreply.github.com>
2021-10-31 15:04:11 -0400

    Added DB table creation scripts

---
+++ DatabaseScripts/CREATE_department.sql
@@ -0,0 +1,6 @@
+CREATE TABLE `department` (
+  `id` char(36) NOT NULL,
+  `name` varchar(45) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `name_UNIQUE` (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
