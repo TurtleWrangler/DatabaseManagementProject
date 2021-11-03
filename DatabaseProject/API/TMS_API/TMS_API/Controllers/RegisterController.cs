efd459b180a3140e3c57f5a29140461b9b8b24be
Derek Dunn <derek.dunn001@gmail.com>
2021-11-03 16:08:28 -0400

    Added POST Register

--- DatabaseProject/API/TMS_API/TMS_API/Controllers/RegisterController.cs
+++ DatabaseProject/API/TMS_API/TMS_API/Controllers/RegisterController.cs
@@ -14,12 +14,25 @@ namespace TMS_API.Controllers
         private static MySqlConnection Connection = new MySqlConnection("server=173.90.136.43;user=brandon;database=tms;port=3306;password=P@ssw0rd");
 
         [HttpPost]
-        public void Register()
+        public void Register(Employee employee)
         {
             Connection.Open();
-            string sql = "INSERT INTO department (id,name) VALUES('111111111111111111111111111111111111','Humans')";
-            MySqlCommand cmd = new MySqlCommand(sql, Connection);
+            string query = "INSERT INTO employee VALUES(@id, @dept_id, @name_first, @name_last, @occupation, @email, @address, @phone_number, @date_of_birth, @date_of_hire, @date_of_dismissal)";
+            MySqlCommand cmd = new MySqlCommand(query, Connection);
+            cmd.Parameters.Add("@id", MySqlDbType.VarChar, 36).Value = Guid.NewGuid();
+            cmd.Parameters.Add("@dept_id", MySqlDbType.VarChar, 36).Value = employee.DeptID;
+            cmd.Parameters.Add("@name_first", MySqlDbType.VarChar, 20).Value = employee.FirstName;
+            cmd.Parameters.Add("@name_last", MySqlDbType.VarChar, 20).Value = employee.LastName;
+            cmd.Parameters.Add("@occupation", MySqlDbType.VarChar, 45).Value = employee.Occupation;
+            cmd.Parameters.Add("@email", MySqlDbType.VarChar, 45).Value = employee.Email;
+            cmd.Parameters.Add("@address", MySqlDbType.VarChar, 50).Value = employee.Address;
+            cmd.Parameters.Add("@phone_number", MySqlDbType.VarChar, 10).Value = employee.PhoneNumber;
+            cmd.Parameters.Add("@date_of_birth", MySqlDbType.Date, 36).Value = employee.DateOfBirth;
+            cmd.Parameters.Add("@date_of_hire", MySqlDbType.Date, 36).Value = employee.DateOfHire;
+            cmd.Parameters.Add("@date_of_dismissal", MySqlDbType.Date, 36).Value = null;
+
             cmd.ExecuteNonQuery();
+            Connection.Close();
         }
     }
 }
