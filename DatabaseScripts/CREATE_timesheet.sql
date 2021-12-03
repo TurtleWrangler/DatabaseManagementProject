71b1c5fb86c7bde94c72fe06de8b9545f58d1772
randobrando1008 <brandongregory1998@gmail.com>
2021-12-02 19:40:48 -0500

    updated SQL scripts

--- DatabaseScripts/CREATE_timesheet.sql
+++ DatabaseScripts/CREATE_timesheet.sql
@@ -5,4 +5,4 @@ CREATE TABLE `timesheet` (
   PRIMARY KEY (`week_start_date`,`employee_id`),
   KEY `employee_id_idx` (`employee_id`),
   CONSTRAINT `employee_id` FOREIGN KEY (`employee_id`) REFERENCES `employee` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
